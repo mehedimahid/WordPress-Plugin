50d488704bca8fec6c3fc8f500fa5de1ea511812
mehedimahid <53916831+mehedimahid@users.noreply.github.com>
2024-08-30 00:36:31 +0600

    another drop down metabox

---
+++ blog/wp-content/plugins/Our Metabox/Drop Down Box/another drop down box/assets/admin/js/main.js
@@ -0,0 +1,9 @@
+;(function($){
+    $(document).ready(function(){
+        $(".book_pby").datepicker({
+            changeMonth: true,
+            changeYear: true
+        });
+    })
+}(jQuery))
+//https://jqueryui.com/datepicker/
