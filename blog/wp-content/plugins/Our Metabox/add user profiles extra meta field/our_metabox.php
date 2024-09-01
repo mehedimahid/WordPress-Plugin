99028e63980d7d0ddf01b167f67738f60c661d4b
mehedimahid <53916831+mehedimahid@users.noreply.github.com>
2024-09-02 00:01:58 +0600

    Add user profiles extra metafield

---
+++ blog/wp-content/plugins/Our Metabox/add user profiles extra meta field/our_metabox.php
@@ -0,0 +1,365 @@
+<?php
+/**
+ * Plugin Name: Our MetaBox
+ * Plugin URI:
+ * Description:Metabox API
+ * Requires at least: 6.4
+ * Requires PHP: 7.2
+ * Version: 3.2.0
+ * Author:Mehedi Hasan
+ * Author URI:
+ * License: GPLv2 or later
+ * License URI:
+ * Text Domain: our-metabox
+ * Domain Path:/languages/
+ * @package our-metabox
+ */
+class OurMetaBox {
+	public function __construct() {
+		add_action("plugins_loaded", array($this, "omb_load_textdomain"));
+		add_action("admin_menu", array($this, "omb_admin_menu"));
+		add_action("save_post", array($this, "omb_save_post_location"));
+		add_action("save_post", array($this, "omb_save_image"));
+		add_action("save_post", array($this, "omb_save_gallery"));
+
+
+		add_action("admin_enqueue_scripts",array($this, "omb_admin_assets"));
+//		add_action("admin_enqueue_scripts",array($this, "omb_admin_assets"));
+
+		add_filter("user_contactmethods", array($this, "omb_user_contact_methods"));
+	}
+	//  //add user  profile's extra  meta field(social link)
+	function omb_user_contact_methods($methods){
+		$methods['facebook'] = __('Facebook', 'our-metabox');
+		$methods['twitter'] = __('Twitter', 'our-metabox');
+		$methods['linked in'] = __('Linked In', 'our-metabox');
+		return $methods;
+	}
+	function omb_load_textdomain(){
+		load_plugin_textdomain('our-metabox', false, basename(dirname(__FILE__)) . '/languages');
+	}
+	function omb_admin_assets(){
+		wp_enqueue_style('omb-admin-style', plugin_dir_url( __FILE__).'assets/admin/css/style.css', null,time());
+		wp_enqueue_style('jq-ui-style', '//code.jquery.com/ui/1.14.0/themes/base/jquery-ui.css', null,time());
+		wp_enqueue_script('omb-admin-script', plugin_dir_url( __FILE__).'assets/admin/js/main.js', array('jquery','jquery-ui-datepicker'),time(),true);
+
+	}
+	private function is_secured($nonce_field,$action,$post_id){
+		$nonce = isset($_POST[$nonce_field]) ?($_POST[$nonce_field]) : '';
+		if($nonce==''){
+			return false;
+		}
+		if(!wp_verify_nonce($nonce,$action)){
+			return false;
+		}
+		if(!current_user_can('edit_post',$post_id)){
+			return false;
+		}
+		if(wp_is_post_autosave($post_id)){
+			return false;
+		}
+		if(wp_is_post_revision($post_id)){
+			return false;
+		}
+
+		return true;
+
+	}
+	// // upload single Image save
+	function omb_save_image($post_id){
+		if(!$this->is_secured("omb_image_nonce","omb_image",$post_id)){
+			return $post_id;
+		}
+		$image_id = isset($_POST["omb_image_id"]) ?($_POST["omb_image_id"]) : '';
+		$image_url = isset($_POST["omb_image_url"]) ?($_POST["omb_image_url"]) : '';
+		update_post_meta($post_id,"omb_image_id",$image_id);
+		update_post_meta($post_id,"omb_image_url",$image_url);
+	}
+	//multiple image save
+	function omb_save_gallery($post_id){
+		if(!$this->is_secured("omb_gallery_nonce","omb_gallery",$post_id)){
+			return $post_id;
+		}
+		$image_id = isset($_POST["omb_images_id"]) ?($_POST["omb_images_id"]) : '';
+		$image_url = isset($_POST["omb_images_url"]) ?($_POST["omb_images_url"]) : '';
+		update_post_meta($post_id,"omb_images_id",$image_id);
+		update_post_meta($post_id,"omb_images_url",$image_url);
+	}
+
+	function omb_save_post_location($post_id){
+		if(!$this->is_secured("omb_location_field","omb_location",$post_id)){
+			return $post_id;
+		}
+		$location = isset($_POST["omb_location"]) ?($_POST["omb_location"]) : '';
+		$country1 = isset($_POST["omb_country1"]) ?($_POST["omb_country1"]) : '';
+		$is_favorite = isset($_POST["omb_is_favorite"]) ?($_POST["omb_is_favorite"]) : 0;
+		$colors = isset($_POST["omb_color"]) ?($_POST["omb_color"]) : array();
+		$color = isset($_POST["omb_clr"]) ?($_POST["omb_clr"]) : '';
+		$dp_color = isset($_POST["omb_dp_color"]) ?($_POST["omb_dp_color"]) : '';
+		$country2 = isset($_POST["omb_country"]) ? sanitize_text_field($_POST["omb_country"]) : '';
+		if($location == ""){
+			return $post_id;
+		}
+		if($country1== ""){
+			return $post_id;
+		}
+		$location = sanitize_text_field($location);
+		$country1 = sanitize_text_field($country1);
+		update_post_meta($post_id, "omb_location", $location);
+		update_post_meta($post_id, "omb_country1", $country1);
+		update_post_meta($post_id, "omb_is_favorite", $is_favorite);
+		update_post_meta($post_id, "omb_color", $colors);
+		update_post_meta($post_id, "omb_clr", $color);
+		update_post_meta($post_id, "omb_dp_color", $dp_color);
+		update_post_meta($post_id, "omb_country", $country2);
+	}
+
+	function omb_admin_menu(){
+		add_meta_box(
+			"omb_post_location",
+			__("Location", "our-metabox"),
+			array($this, "omb_display_metabox"),
+			array("post","page")//where show this meta box
+		);
+		//Meta Box Styling
+		add_meta_box(
+			"omb_book_info",
+			__("Book Info", "our-metabox"),
+			array($this, "omb_book_info"),
+			array("book")//where show this meta box
+		);
+		add_meta_box(
+			"omb_image_info",
+			__("Upload Image", "our-metabox"),
+			array($this, "omb_image_info"),
+			array("post")//where show this meta box
+		);
+		add_meta_box(
+			"omb_gallery_info",
+			__("Gallery Image", "our-metabox"),
+			array($this, "omb_gallery_info"),
+			array("post")//where show this meta box
+		);
+	}
+	//Meta Box Styling
+	function omb_book_info(){
+			wp_nonce_field('omb_book','omb_book_nonce');
+			$metabox_html = <<<HTML
+				<div class="field">
+					<div class="field_c">
+						<div class="label_c" >
+							<label for="book_author">Book Author:</label>
+						</div>
+						<div class="input_c"> 
+							<input type="text" class="widefat" id="book_author" />
+						</div>
+						<div class="float_c"></div>
+					</div>
+					<div class="field_c">
+						<div class="label_c" >
+							<label for="book_isbn">Book ISBNr:</label>
+						</div>
+						<div class="input_c">
+							<input type="text" id="book_isbn" />
+						</div>
+						<div class="float_c"></div>
+					</div>
+					<div class="field_c">
+						<div class="label_c" >
+							<label for="book_pby">Publish year:</label>
+						</div>
+						<div class="input_c">
+							<input type="text" id="book_pby" class="book_pby" />
+						</div>
+						<div class="float_c"></div>
+					</div>
+				</div>
+			HTML;
+			echo $metabox_html;
+
+	}
+	// // Upload single Image
+	function omb_image_info($post){
+		$image_id = esc_attr(get_post_meta($post->ID,"omb_image_id",true));
+		$image_url =esc_attr(get_post_meta($post->ID,"omb_image_url",true));
+			wp_nonce_field('omb_image','omb_image_nonce');
+			$metabox_html = <<<HTML
+				<div class="field">
+					<div class="field_c">
+						<div class="label_c" >
+							<label >Upload Image:</label>
+						</div>
+						<div class="input_c"> 
+							<button type="button" id="upload_image">Upload Image</button>
+							<input type="hidden" name="omb_image_id" id="omb_image_id" value="$image_id" />
+							<input type="hidden" name="omb_image_url" id="omb_image_url" value="$image_url" />
+							<div id="image_container"></div>
+						</div>
+						<div class="float_c"></div>
+					</div>
+				</div>		
+			HTML;
+			echo $metabox_html;
+
+	}
+	//upload multiple image
+	function omb_gallery_info($post){
+		$image_id = esc_attr(get_post_meta($post->ID,"omb_images_id",true));
+		$image_url =esc_attr(get_post_meta($post->ID,"omb_images_url",true));
+			wp_nonce_field('omb_gallery','omb_gallery_nonce');
+			$label = __("Gallery", "our-metabox");
+			$button_label = __("Choose Images", "our-metabox");
+			$metabox_html = <<<HTML
+				<div class="field">
+					<div class="field_c">
+						<div class="label_c" >
+							<label >{$label}:</label>
+						</div>
+						<div class="input_c"> 
+							<button type="button" id="upload_images">{$button_label}</button>
+							<input type="hidden" name="omb_images_id" id="omb_images_id" value="$image_id" />
+							<input type="hidden" name="omb_images_url" id="omb_images_url" value="$image_url" />
+							<div id="images_container"></div>
+						</div>
+						<div class="float_c"></div>
+					</div>
+				</div>		
+			HTML;
+			echo $metabox_html;
+
+	}
+	function omb_display_metabox($post){
+		$location = get_post_meta($post->ID, "omb_location", true);
+		$country = get_post_meta($post->ID, "omb_country1", true);
+		$is_checked = get_post_meta($post->ID, "omb_is_favorite", true);
+		$save_colors = get_post_meta($post->ID, "omb_color",true);
+		$save_color = get_post_meta($post->ID, "omb_clr",true);
+		$dp_color = get_post_meta($post->ID, "omb_dp_color",true);
+		$check_country = get_post_meta($post->ID, "omb_country",true);
+
+		$checked = $is_checked ? "checked" : "";
+		$colors = [ 'red' , 'green' ,'blue','yellow','black','white'];
+		$label1 = __("Location", "our-metabox");
+		$label2 = __("Country", "our-metabox");
+		$label3 = __("Is Favorite", "our-metabox");
+		$label4 = __("Color", "our-metabox");
+		$label5 = __("Country", "our-metabox");
+
+		$countries = [
+			"Afghanistan","Bangladesh","Bhutan","India","Nepal","Sri Lanka","Maldives","Pakistan"];
+
+		wp_nonce_field("omb_location", "omb_location_field");
+		$metabox_html = <<<EOD
+			<p>
+				<label for="omb_location">{$label1}</label>
+				<input type="text" id="omb_location" name="omb_location" value="{$location}" />
+			</p>
+			<p>
+				<label for="omb_country1">{$label2}</label>
+			
+				<input type="text" id="omb_country1" name="omb_country1" value="{$country}" />
+			</p>
+			<p>
+				<label for="omb_is_favorite">{$label3}:</label>
+				<input type="checkbox" id="omb_is_favorite" name="omb_is_favorite" value="1" {$checked}/>
+			</p>
+			<p>
+				<label >{$label4}:</label>
+
+		EOD;
+//Multiple CheckBox
+		foreach($colors as $color){
+			$_color = ucwords($color);
+			$checked = in_array($color,$save_colors)?"checked":'';
+			$metabox_html .= <<<EOD
+				<label for="omb_color_{$color}">{$_color}</label>
+				<input type="checkbox" id="omb_color_{$color}" name="omb_color[]" value="{$color}" {$checked}/>
+					
+			EOD;
+		}
+
+
+		$metabox_html .= "</p>";
+		//Radio Box
+		$metabox_html .=<<<EOD
+			<p>
+				<label >{$label4}:</label>
+		EOD;
+
+		foreach($colors as $color) {
+			$_color = ucwords($color);
+			$checked = ($color == $save_color) ? "checked='checked'" : '';
+			$metabox_html .= <<<EOD
+        <label for="omb_clr_{$color}">{$_color}</label>
+        <input type="radio" id="omb_clr_{$color}" name="omb_clr" value="{$color}" {$checked}/>
+    EOD;
+		}
+
+		$metabox_html .= "</p>";
+// //DropDown Box
+//		$metabox_html .= <<<EOD
+//    <p>
+//        <label>{$label5}:</label>
+//        <select name="omb_country" id="omb_country">
+//EOD;
+//
+//		foreach($countries as $country) {
+//
+//			$_country = ucwords($country);
+////
+//			$selected = ($country == $check_country) ? "selected='selected'" : '';
+//			$metabox_html .= <<<EOD
+//        <option id="omb_country_{$country}" value="{$country}" {$selected}>{$_country}</option>
+//    EOD;
+//		}
+//
+//		$metabox_html .= <<<EOD
+//        </select>
+//    </p>
+//EOD;
+	// //or
+		$dropdown_html = "<option value='0'>".__('Select a color','our-metabox')."</option>";
+		foreach($colors as $color) {
+			$checked = ($color == $dp_color) ? "selected='selected'" : '';
+			$dropdown_html .= sprintf("<option value='%s' %s>%s</option>",$color,$checked,$color);
+		}
+		$metabox_html .= <<<EOD
+<p>
+        <label>{$label4}:</label>
+        <select name="omb_dp_color" id="omb_dp_color">
+        {$dropdown_html}
+</select>
+EOD;
+
+		echo $metabox_html;
+
+	}
+
+}
+
+new OurMetaBox();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
