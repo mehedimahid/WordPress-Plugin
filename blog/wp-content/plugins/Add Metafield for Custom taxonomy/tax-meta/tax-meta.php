640bf025d147c73380c10cd9df656d467df4818b
mehedimahid <53916831+mehedimahid@users.noreply.github.com>
2024-09-05 11:45:34 +0600

    Add Meta Field for Custom taxonomy

--- blog/wp-content/plugins/Add Metafield for Custom taxonomy/tax-meta/tax-meta.php
+++ blog/wp-content/plugins/Add Metafield for Custom taxonomy/tax-meta/tax-meta.php
@@ -30,20 +30,21 @@ function taxm_bootstram() {
 	));
 }
 add_action('init', 'taxm_bootstram');
-
+//add taxonomy and show
 function taxm_add_category_form_fields($term_id) {
 	$extra_info = get_term_meta($term_id, "texm_extra_info", true);
 	?>
     <div class="form-field form-required term-name-wrap">
         <label for="extra-info"><?php _e("Extra info", "tax-meta"); ?></label>
         <input name="extra-info" id="extra-info" type="text" value="<?php echo esc_attr($extra_info); ?>" size="40" aria-required="true" aria-describedby="name-description">
-        <p id="name-description"><?php _e("* add your taxonomy", "tax-meta"); ?></p>
+        <p id="name-description"><?php _e("*  write something about your taxonomy", "tax-meta"); ?></p>
     </div>
 	<?php
 }
 add_action("category_add_form_fields", "taxm_add_category_form_fields");
 add_action('post_tag_add_form_fields', 'taxm_add_category_form_fields');
-
+add_action('genre_add_form_fields', 'taxm_add_category_form_fields');
+//edit taxonomy
 function taxm_edit_category_form_fields($term) {
 	$extra_info = get_term_meta($term->term_id, "texm_extra_info", true);
 	?>
@@ -55,14 +56,15 @@ function taxm_edit_category_form_fields($term) {
         </th>
         <td>
             <input name="extra-info" id="extra-info" type="text" value="<?php echo esc_attr($extra_info); ?>" size="40" aria-required="true" aria-describedby="name-description">
-            <p id="name-description"><?php _e("* add your taxonomy", "tax-meta"); ?></p>
+            <p id="name-description"><?php _e("* write something about your taxonomy", "tax-meta"); ?></p>
         </td>
     </tr>
 	<?php
 }
 add_action("category_edit_form_fields", "taxm_edit_category_form_fields");
 add_action('post_tag_edit_form_fields', 'taxm_edit_category_form_fields');
-
+add_action('genre_edit_form_fields', 'taxm_edit_category_form_fields');
+//save taxonomy
 function taxm_save_category_meta($term_id) {
 	if (isset($_POST['_wpnonce_add-tag']) && wp_verify_nonce($_POST['_wpnonce_add-tag'], 'add-tag')) {
 		$extra_info = sanitize_text_field($_POST['extra-info']);
@@ -71,7 +73,8 @@ function taxm_save_category_meta($term_id) {
 }
 add_action('create_category', "taxm_save_category_meta");
 add_action('create_post_tag', 'taxm_save_category_meta');
-
+add_action('create_genre', 'taxm_save_category_meta');
+//update taxonomy
 function taxm_update_category_meta($term_id) {
 	if (isset($_POST['_wpnonce']) && wp_verify_nonce($_POST['_wpnonce'], "update-tag_{$term_id}")) {
 		$extra_info = sanitize_text_field($_POST['extra-info']);
@@ -80,3 +83,8 @@ function taxm_update_category_meta($term_id) {
 }
 add_action('edit_category', 'taxm_update_category_meta');
 add_action('edit_post_tag', 'taxm_update_category_meta');
+add_action('edit_genre', 'taxm_update_category_meta');
+////1st action hook for category
+/// 2nd action hook for post tag
+/// 3rd action hook for custom taxonomy
+///
